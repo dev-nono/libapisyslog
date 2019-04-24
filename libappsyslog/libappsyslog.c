50ab8e00a8dbd10b3dab1106041ce4c2c47b0930
bruno <bruno@osboxes>
2019-04-24 10:00:08 -0400

    new project

---
+++ libappsyslog/libappsyslog.c
@@ -0,0 +1,16 @@
+/*
+ * appsyslog.c
+ *
+ *  Created on: Apr 17, 2019
+ *      Author: bruno
+ */
+
+
+int readConfig(const char* a_ConfigFilemame)
+{
+	int result = 0;
+
+
+	return result;
+}
+
---
+++ libappsyslog/libappsyslog.h
@@ -0,0 +1,13 @@
+/*
+ * libappsyslog.h
+ *
+ *  Created on: Apr 17, 2019
+ *      Author: bruno
+ */
+
+#ifndef LIBAPPSYSLOG_H_
+#define LIBAPPSYSLOG_H_
+
+int readConfig(const char* a_ConfigFilemame);
+
+#endif /* LIBAPPSYSLOG_H_ */
