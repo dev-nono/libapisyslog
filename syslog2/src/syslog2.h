1ad3ca6b3c49ef1475f360b8482ad7c4fb6424d4
bstatuto <bruno.statuto@gmail.com>
2022-11-21 16:55:55 +0000

    save

--- syslog2/src/syslog2.h
+++ syslog2/src/syslog2.h
@@ -43,6 +43,25 @@
 //     __attribute__ ((__format__ (__printf__, 2, 0)));
 //#endif
 
+/*************************************************
+ DATE 	TIMESTAMP 	HOSTNAME  	PROCESS_NAME PID	MSG
+ Nov 21 15:24:38 	buildsystem nmbd[594]:   name_to_nstring: workgroup name USR.INGENICO.LOC is too long. Truncating to
+ Nov 21 15:24:48 	buildsystem nmbd[594]: [2022/11/21 15:24:48.031550,  0] ../source3/nmbd/nmbd_workgroupdb.c:57(name_to_unstring)
+
+ DATE 	TIMESTAMP 	HOSTNAME  	PROCESS_NAME PID	FUNCTIOn IN/OUT	MSG...
+
+
+ */
+
+#define LOG_MS				(1<<0)
+#define LOG_MICRO			(1<<1)
+#define LOG_NANO			(1<<2)
+#define LOG_FUNCTION		(1<<3)
+#define LOG_IN				(1<<4)
+#define LOG_OUT				(1<<5)
+
+
+
 #undef syslog
 
 
